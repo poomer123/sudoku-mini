22f023f18ada1c6c4500a5d1dfb18a8c1f19878a
zobry <apirat.chiwa@gmail.com>
2019-06-11 21:26:19 +0700

    show number 1-4

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
   return (
     <div className="App">
       <div className="board">
-        <Cell number={2} />
-        <Cell number={2} />
-        <Cell number={2} />
-        <Cell number={2} />
-        <Cell number={2} />
-        <Cell number={2} />
-        <Cell number={2} />
-        <Cell number={2} />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
       </div>
     </div>
   );
--- src/Cell.js
+++ src/Cell.js
@@ -1,8 +1,22 @@
 import React, { Component } from "react";
 
 class Cell extends Component {
+  state = {
+    number: 0
+  };
   render() {
-    return <div className="cell">{this.props.number}</div>;
+    return (
+      <div
+        className="cell"
+        onClick={e => {
+          this.setState({
+            number: (this.state.number + 1) % 5
+          });
+        }}
+      >
+        {this.state.number !== 0 && this.state.number}
+      </div>
+    );
   }
 }
 
