a9fe606d849938f5bdd700ff404f8c443fb06d25
zobry <apirat.chiwa@gmail.com>
2019-06-18 21:44:37 +0700

    submit board

--- src/Board.js
+++ src/Board.js
@@ -14,24 +14,29 @@ class Board extends Component {
             [true, true, false, false],
             [true, false, true, false],
             [true, false, false, true]
-        ]
+        ],
+        statusText: ''
     }
     render() {
         return (
-            <div className="board">
-                {
-                    this.state.board.map((row, i) => (
-                        row.map((number, j) => (
-                            <Cell
-                                key={`cell-${i}-${j}`}
-                                number={number}
-                                isInitial={this.state.initial[i][j]}
-                                onChange={(newNumber) => this.onChange(newNumber, i, j)}
-                            />
+            <div>
+                <div className="board">
+                    {
+                        this.state.board.map((row, i) => (
+                            row.map((number, j) => (
+                                <Cell
+                                    key={`cell-${i}-${j}`}
+                                    number={number}
+                                    isInitial={this.state.initial[i][j]}
+                                    onChange={(newNumber) => this.onChange(newNumber, i, j)}
+                                />
+                            ))
                         ))
-                    ))
 
-                }
+                    }
+                </div>
+                <button onClick={() => this.submit()}>Submit</button>
+                <p>{this.state.statusText}</p>
             </div>
         )
     }
@@ -42,6 +47,36 @@ class Board extends Component {
         this.setState({
             board
         })
+    }
+
+    submit() {
+        const isValid = this.validate(this.state.board)
+        this.setState({
+            statusText: isValid ? 'Complete' : 'In Valid'
+        })
+    }
+
+    validate(board) {
+        let isValid = true
+        for (let i = 0; i < 4; i++) {
+            const horizontal = new Set()
+            const vertical = new Set()
+            const square = new Set()
+            for (let j = 0; j < 4; j++) {
+                horizontal.add(board[i][j])
+                vertical.add(board[j][i])
+                square.add(
+                    board[2 * (i % 2) + (j % 2)][2 * Math.floor(i / 2) + Math.floor(j / 2)]
+                )
+            }
+            horizontal.delete(0)
+            vertical.delete(0)
+            square.delete(0)
+            if (horizontal.size !== 4 || vertical.size !== 4 || square.size !== 4) {
+                isValid = false;
+            }
+        }
+        return isValid
     }
 }
 
